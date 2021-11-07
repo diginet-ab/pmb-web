128f5605eca497d4057b0f9c3b9847e4c5e79c9a
Anders Westberg <anders.westberg@diginet.se>
2021-11-07 14:19:59 +0100

    Build updated

---
+++ build/precache-manifest.5fb8d4af5908869b14fed34489cffda5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f274e679a149c86f8f64b3448a6c182",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6295abc8a65863ee3b6f",
+    "url": "./static/css/2.210513a5.chunk.css"
+  },
+  {
+    "revision": "8818f5e05816b67dcd60",
+    "url": "./static/css/main.4cfab566.chunk.css"
+  },
+  {
+    "revision": "6295abc8a65863ee3b6f",
+    "url": "./static/js/2.544bbf32.chunk.js"
+  },
+  {
+    "revision": "8818f5e05816b67dcd60",
+    "url": "./static/js/main.a8c03248.chunk.js"
+  },
+  {
+    "revision": "56b02116b0859a68ac09",
+    "url": "./static/js/runtime-main.75c8c25c.js"
+  },
+  {
+    "revision": "c7ea41792dbc1187e92337286c125f90",
+    "url": "./static/media/NIBE_AirSite_vector_logo.c7ea4179.svg"
+  }
+]);
